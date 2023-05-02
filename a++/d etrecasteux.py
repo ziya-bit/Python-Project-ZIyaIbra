3f7c5e0bbf09085f84925a84a91743648fb253bd
ziya-bit <m.ziyaibra@gmail.com>
2023-05-02 00:42:50 -0700

    uralsk

--- a++/d etrecasteux.py
+++ a++/d etrecasteux.py
@@ -1,7 +1,12 @@
 from turtle import *
 from time import *
+from random import *
 speed(0)
 for i in range(360):
+    r=randint(0, 255)
+    g=randint(0, 255)
+    b=randint(0, 255)
+    pencolor((r, g, b))
     forward(100)
     for i in range(3):
         backward(25)
